07f45cf2f2f14b2baf5e4a491342e19eabd6b77e
Jeff Andersen <bluegate010@gmail.com>
2013-11-24 01:53:30 -0700

    Added jeospatial library

---
+++ cs450-hymns/src/mp3/MP3.java
@@ -0,0 +1,12 @@
+package mp3;
+
+import java.io.File;
+
+import org.mp3transform.wav.WavConverter;
+
+public class MP3 {
+	public static void convertToWav(File inputFile) {
+		WavConverter c = new WavConverter();
+		
+	}
+}
